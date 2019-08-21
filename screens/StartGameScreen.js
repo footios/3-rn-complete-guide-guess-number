471c7225a12edb401ae884714ba5b203070a3efa
Fotis Tsakiris <footios76@gmail.com>
2019-08-22 02:40:17 +0300

    StartGameScreen added

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { View, StyleSheet, Text, TextInput, Button } from 'react-native';
+
+const StartGameScreen = props => {
+    return (
+        <View style={styles.screen} >
+            <Text style={styles.title} >Start a New Game!</Text>
+            <View style={styles.inputContainer} >
+                <Text>Select a Number</Text>
+                <TextInput style={{fontSize: 18, borderColor: 'gray', width: '30%', height: '20%', borderWidth: 1}} />
+                <View style={styles.buttonContainer} >
+                    <Button title='Reset' onPress={() => {}} />
+                    <Button title='Confirm' onPress={() => {}} />
+                </View>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center',
+    }, 
+    title: {
+        fontSize: 20,
+        marginVertical: 10,
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center',
+        shadowColor: 'black',
+        shadowOffset: {
+            width: 0, height: 2
+        },
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        backgroundColor: 'white',
+        // elevation (android) uses the default from material design
+        elevation: 5,
+        padding: 10,
+        borderRadius: 10,
+        // target individual corners with borderBottomLeftRadius etc.
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        padding: 15
+    }
+
+})
+
+export default StartGameScreen;
