3c2ec2e010a9e2b34fb32e0faeb57bf063573794
Fotis Tsakiris <footios76@gmail.com>
2019-08-23 13:35:37 +0300

    Success!!!

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -5,41 +5,20 @@ import NumberContainer from '../components/NumberContainer';
 import Card from '../components/Card';
 
 // outside because it shouldn't be recreated in every rerendering...
-// The function is working fine and it finds the right number automatically.
-// So there is no need to give hints.
-// The logic off the app is different, so this cannot be applied.
-const findNumber = function(min, max, x) {
-	let counter = 0
-	
-	let num = 0;
-	let start = min,
-	  end = max;
-  
-	// Iterate while start not meets end
-	while (start <= end) {
-	  // Find the mid index
-	  
-	  let mid = Math.floor((start + end) / 2);
-  
-	  // If element is present at mid, return True
-	  if (mid === x) {
-		
-		num = mid;
-		return num;
-	  }
-	  // Else look in left or right half accordingly
-	  else if (mid < x) start = mid + 1;
-	  else end = mid - 1;
-  console.log("mid", mid, "start", start, end)
-  counter++;
-	 console.log('counter', counter)
-	}
-	return num;
-  };
 
 const GameScreen = (props) => {
-
-	const [ currentGuess, setCurrentGuess ] = useState(findNumber(1, 100, props.userChoice));
+	const getMiddleNumber = function(min, max, exclude) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+	
+		let middleNumber = Math.floor((min + max) / 2);
+		if (middleNumber === exclude) {
+			middleNumber = Math.floor(Math.random() * (max - min) + min)
+		};
+		  return middleNumber; 
+	}
+	
+	const [ currentGuess, setCurrentGuess ] = useState(getMiddleNumber(1, 100, props.userChoice));
 	const [ rounds, settRounds ] = useState(0);
 
 	const currentLow = useRef(1);
@@ -47,9 +26,13 @@ const GameScreen = (props) => {
 
 	const { userChoice, onGameOver } = props;
 
+	if (currentGuess === props.userChoice) {
+		props.onGameOver(rounds);
+	} 
+
 	useEffect(
 		() => {
-			if (currentGuess === props.userChoice) {
+			if (currentGuess === props.userChoice ) {
 				props.onGameOver(rounds);
 			}
 		},
@@ -72,10 +55,12 @@ const GameScreen = (props) => {
 		} else {
 			currentLow.current = currentGuess;
 		}
-		const nextNumber = findNumber(currentLow.current, currentHigh.current, currentGuess);
+		const nextNumber = getMiddleNumber(currentLow.current, currentHigh.current, currentGuess);
 		setCurrentGuess(nextNumber);
 		settRounds((curRounds) => curRounds + 1);
 	};
+
+	
 	return (
 		<View style={styles.screen}>
 			<Text>Opponent's Guess: </Text>
