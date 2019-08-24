16d5628643400dd8662e79f92695b62a2e830889
Fotis Tsakiris <footios76@gmail.com>
2019-08-24 11:18:20 +0300

    fetch fonts with AppLoading component

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
 import GameOverScreen from './screens/GameOverScreen';
 
+const fetchFonts = () => {
+	// returns a promise
+	return Font.loadAsync({
+		'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+		'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+	});
+};
+
 export default function App() {
 	const [ userNumber, setUserNumber ] = useState();
 	const [ guessRounds, setGuessRounds ] = useState(0);
+	const [ dataLoaded, setDataLoaded ] = useState(false);
+
+	if (!dataLoaded) {
+		// AppLoading prolongs the splash screen...
+		return <AppLoading 
+		startAsync={fetchFonts} // point at operation to start on 1st render
+		onFinish={() => setDataLoaded(true)} 
+		onError={() => console.log(err)} />
+	}
 
 	const configureNewGameHandler = () => {
 		setGuessRounds(0);
@@ -19,7 +38,7 @@ export default function App() {
 		setUserNumber(selectedNumber);
 	};
 
-	const  gameOverHandler = (numOfRounds) => {
+	const gameOverHandler = (numOfRounds) => {
 		setGuessRounds(numOfRounds);
 	};
 
