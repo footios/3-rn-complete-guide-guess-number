dd4814d9f48ff023dac7554ca4f9a495461b811e
Fotis Tsakiris <footios76@gmail.com>
2019-08-24 14:11:29 +0300

    image added

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import { View, StyleSheet, Image, Button } from 'react-native';
 
 import BodyText from '../components/BodyText'
 import TitleText from '../components/TitleText'
@@ -9,6 +9,9 @@ const GameOverScreen = (props) => {
 	return (
 		<View style={styles.screen}>
 			<TitleText>Game Over!</TitleText>
+			<View style={styles.imageContainer} >
+			<Image style={styles.image} resizeMode={'cover'} source={require('../assets/success.png')} />
+			</View>
 			<BodyText>Number of rounds: {props.roundsNumber} </BodyText>
 			<BodyText>Number was: {props.userNumber} </BodyText>
 			<Button title="NEW GAME" onPress={props.onRestart} />
@@ -21,6 +24,22 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center'
+	}, 
+	imageContainer: {
+		// in order to have a perfect circle in android
+		// we need to put the width and heigt to be equal
+		// and the borderRadius at half of their value
+		width: 300,
+		height: 300,
+		borderRadius: 150,
+		borderWidth: 3,
+		borderColor: 'black',
+		overflow: 'hidden',
+		margin: 10
+	},
+	image: {
+		width: '100%',
+		height: '100%'
 	}
 });
 
